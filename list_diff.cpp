719051b11d4ea3f7fc5da8a38b3d7e6f0b778d17
OwOb <35055831+OwOb@users.noreply.github.com>
2018-11-28 02:28:37 +0800

    Add files via upload

---
+++ list_diff.cpp
@@ -0,0 +1,214 @@
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int getAllFile(char *_path) {
+
+	//open path
+	DIR *_path_dp = opendir(_path);
+
+	//if path is not exist
+	if (_path_dp == NULL) {
+		return -1;
+	}
+
+	int _path_length = strlen(_path);
+	dirent *_dirp;
+
+	//traverse change_path
+	while ((_dirp = readdir(_path_dp)) != NULL) {
+
+		//subdirentory is "." or ".."
+		if (strcmp(_dirp->d_name, ".") == 0 || strcmp(_dirp->d_name, "..") == 0) {
+			continue;
+		}
+
+		//if dirp is direntory
+		if (_dirp->d_type == DT_DIR) {
+
+			//count and set length of path name
+			int _subdirentory_path_length = _path_length + 1 + strlen(_dirp->d_name);
+			char *_subdirentory_path = (char*)malloc((_subdirentory_path_length+1) * sizeof(char));
+
+			//malloc failure
+			if (_subdirentory_path == NULL) {
+				closedir(_path_dp);
+				return -2;
+			}
+
+			//set subdirentory path
+			sprintf(_subdirentory_path, "%s/%s", _path, _dirp->d_name);
+
+			//recur subdirentory
+			getAllFile(_subdirentory_path);
+
+			//free memory
+			free(_subdirentory_path);
+		}
+
+		//if dirp is file
+		else {
+			//print the file name
+			printf("%s/%s\n", _path ,_dirp->d_name);
+		}
+
+	}
+
+	//clsoe DIR
+	closedir(_path_dp);
+
+	return 0;	//success
+}
+
+int getDifferentFiles(char *_original_path, char *_change_path) {
+
+	//open path
+	DIR *_original_dp = opendir(_original_path);
+	if (_original_dp == NULL) {
+		return -1;
+	}
+	DIR *_change_dp = opendir(_change_path);
+	if (_change_dp == NULL) {
+		closedir(_original_dp);
+		return -1;
+	}
+
+
+	int _original_path_length = strlen(_original_path);
+	int _change_path_length = strlen(_change_path);
+	dirent *_dirp;
+
+	//traverse change_path
+	while ((_dirp = readdir(_change_dp)) != NULL) {
+
+		//subdirentory is "." or ".."
+		if (strcmp(_dirp->d_name, ".") == 0 || strcmp(_dirp->d_name, "..") == 0) {
+			continue;
+		}
+
+		//if dirp is direntory
+		if (_dirp->d_type == DT_DIR) {
+
+			//count and set length of path name
+			int _subdirentory_name_length = strlen(_dirp->d_name);
+
+			int _original_subdirentory_path_length = _original_path_length + 1 + _subdirentory_name_length;
+			int _change_subdirentory_path_length = _change_path_length + 1 + _subdirentory_name_length;
+
+			char *_original_subdirentory_path = (char*)malloc((_original_subdirentory_path_length+1) * sizeof(char));
+			char *_change_subdirentory_path = (char*)malloc((_change_subdirentory_path_length+1) * sizeof(char));
+
+			//malloc failure
+			if (_original_subdirentory_path == NULL || _change_subdirentory_path == NULL) {
+				free(_original_subdirentory_path);
+				free(_change_subdirentory_path);
+				closedir(_original_dp);
+				closedir(_change_dp);
+				return -2;
+			}
+
+			//set subdirentory path
+			sprintf(_original_subdirentory_path, "%s/%s", _original_path, _dirp->d_name);
+			sprintf(_change_subdirentory_path, "%s/%s", _change_path, _dirp->d_name);
+
+			//recur subdirentory
+			getDifferentFiles(_original_subdirentory_path, _change_subdirentory_path);
+
+			//free memory
+			free(_original_subdirentory_path);
+			free(_change_subdirentory_path);
+		}
+
+		//if dirp is file
+		else {
+
+			//count and set length of path name
+			int _file_name_length = strlen(_dirp->d_name);
+			int _original_file_path_length = _original_path_length + 1 + _file_name_length;
+			char *_original_file_path = (char*)malloc((_original_file_path_length+1) * sizeof(char));
+
+			//malloc failure
+			if (_original_file_path == NULL) {
+				closedir(_original_dp);
+				closedir(_change_dp);
+				return -2;
+			}
+
+			//set file path
+			sprintf(_original_file_path, "%s/%s", _original_path, _dirp->d_name);
+
+			//open file in original path
+			FILE *_original_file = fopen(_original_file_path, "r");
+			free(_original_file_path);
+
+			//if original file is exist
+			if (_original_file != NULL) {
+
+				//count and set length of command
+				int _original_file_command_length = 7 + _original_path_length + 1 + _file_name_length;
+				int _change_file_command_length = 7 + _change_path_length + 1 + _file_name_length;
+
+				char *_original_file_command = (char*)malloc((_original_file_command_length+1) * sizeof(char));
+				char *_change_file_command = (char*)malloc((_change_file_command_length+1) * sizeof(char));
+
+				char *_original_file_md5hash = (char*)malloc((_file_name_length+35) * sizeof(char));
+				char *_change_file_md5hash = (char*)malloc((_file_name_length+35) * sizeof(char));
+
+				//malloc failure
+				if (_original_file_command == NULL || _change_file_command == NULL || _original_file_md5hash == NULL || _change_file_md5hash == NULL) {
+					free(_original_file_command);
+					free(_change_file_command);
+					free(_original_file_md5hash);
+					free(_change_file_md5hash);
+					closedir(_original_dp);
+					closedir(_change_dp);
+					return -2;
+				}
+
+				//set file command
+				sprintf(_original_file_command, "md5sum %s/%s", _original_path, _dirp->d_name);
+				sprintf(_change_file_command, "md5sum %s/%s", _change_path, _dirp->d_name);
+
+				//get md5hash
+				fgets(_original_file_md5hash, sizeof(_original_file_md5hash), popen(_original_file_command, "r"));
+				fgets(_change_file_md5hash, sizeof(_change_file_md5hash), popen(_change_file_command, "r"));
+
+				//compare two file
+				//if different then print file
+				if (strcmp(_original_file_md5hash, _change_file_md5hash) != 0) {
+					printf("%s/%s\n", _change_path, _dirp->d_name);
+				}
+
+				//free memory
+				free(_original_file_command);
+				free(_change_file_command);
+				free(_original_file_md5hash);
+				free(_change_file_md5hash);
+
+				//close FILE
+				fclose(_original_file);
+			}
+
+			//original file is not exist
+			else {
+				printf("%s/%s\n", _change_path, _dirp->d_name);
+			}
+
+		}
+
+	}
+
+	//clsoe DIR
+	closedir(_original_dp);
+	closedir(_change_dp);
+
+	return 0; //success
+}
+
+int main() {
+	char original[] = "original_ect";
+	char change[] = "change_ect";
+	getDifferentFiles(original, change);
+	return 0;
+}
